a05f266194a856369ebb8f9a65ee272a70311ca7
Darryl <kwekleedarryl@gmail.com>
2026-02-19 17:23:32 +0800

    Enemies in progress

--- Assets/Scripts/Static Data/RangedEnemyClassData.cs
+++ Assets/Scripts/Static Data/RangedEnemyClassData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "RangedEnemyClassData", menuName = "Scriptable Objects/RangedEnemyClassData")]
 public class RangedEnemyClassData : BaseClassData
 {
-    public CLASSTYPE classType;
-
+    public float attackSpeed;
+    public float moveSpeed;
+    public ENEMYCLASSTYPE enemyClassType;
 }
