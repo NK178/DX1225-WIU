a568d23420154d2938b860182faa29334c4d413c
NK178 <ayhngern@gmail.com>
2026-02-24 11:40:32 +0800

    fruits models yay

--- Assets/Scripts/Items & Inventory/PickupableItems.cs
+++ Assets/Scripts/Items & Inventory/PickupableItems.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PickupableItem : MonoBehaviour
 {
 
     [SerializeField] private ItemData itemData;
-    [SerializeField] private MeshFilter meshFilter; 
+    [SerializeField] private MeshFilter meshFilter;
+
+    public event Action onPickedUpEvent;
+
 
     public void SetMesh(Mesh mesh)
     {
